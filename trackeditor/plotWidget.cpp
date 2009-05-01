de97ba0aa05587099d6328693779893ca0e677b1
Martin Runge <martin.runge@web.de>
2009-05-01 08:51:33 +0000

    plot over time now working

--- trackeditor/plotWidget.cpp
+++ trackeditor/plotWidget.cpp
@@ -19,9 +19,6 @@
 
 
 plotWidget::plotWidget(enum plotTypeX x_type, enum plotTypeY y_type, QWidget * parent) : QwtPlot(parent) {
-    //m_qwtPlot = new QwtPlot(parent);
-    //m_qwtPlot->setObjectName(QString::fromUtf8("m_qwtPlot"));
-    //m_qwtPlot->setGeometry(QRect(20, 40, 511, 200));
 
 	m_x_type = x_type;
 	m_y_type = y_type;
@@ -31,23 +28,6 @@ plotWidget::plotWidget(enum plotTypeX x_type, enum plotTypeY y_type, QWidget * p
     m_grid->setMajPen(QPen(Qt::black, 0, Qt::DotLine));
     m_grid->setMinPen(QPen(Qt::gray, 0 , Qt::DotLine));
     m_grid->attach(this);
-
-    // curves
-    // m_alt_crv = new QwtPlotCurve("Altitude");
-    //m_alt_crv->setRenderHint(QwtPlotItem::RenderAntialiased);
-    // m_alt_crv->setPen(QPen(Qt::blue));
-    // m_alt_crv->setYAxis(QwtPlot::yLeft);
-    // m_alt_crv->attach(this);
-
-    // m_speed_crv = new QwtPlotCurve("Speed");
-    // m_speed_crv->setRenderHint(QwtPlotItem::RenderAntialiased);
-    // m_speed_crv->setPen(QPen(Qt::red));
-    // m_speed_crv->setYAxis(QwtPlot::yRight);
-    // m_speed_crv->attach(this);
-
-    // enableAxis(QwtPlot::yRight);
-
-	// m_curve_list = new QList<QwtPlotCurve*>;
 
     m_curve_list.clear();
 
@@ -66,29 +46,8 @@ plotWidget::plotWidget(enum plotTypeX x_type, enum plotTypeY y_type, QWidget * p
 plotWidget::~plotWidget() {
 	delete m_grid;
 
-	// delete m_alt_crv;
-	// delete m_speed_crv;
-
-	// delete m_curve_list;
-
-	// if( m_alt_data != 0) delete m_alt_data;
-	// if( m_speed_data != 0) delete m_speed_data;
-
 }
 
-//void plotWidget::setTrack(Track* track)
-//{
-//	m_alt_data = new PlotData(track, TYPE_X_DIST, TYPE_Y_ALT, 1000);
-//	m_speed_data = new PlotData(track, TYPE_X_DIST, TYPE_Y_SPEED, 1000);
-//
-//	m_alt_crv->setPen(QPen(track->getColor()));
-//	m_alt_crv->setData(*m_alt_data);
-//	// m_alt_crv->setData(*m_speed_data);
-//	m_speed_crv->setPen(QPen(track->getColor()));
-//	m_speed_crv->setData(*m_speed_data);
-//
-//	setAutoReplot(true);
-//}
 
 void plotWidget::setTracks(QList<Track*> tracks) {
 
